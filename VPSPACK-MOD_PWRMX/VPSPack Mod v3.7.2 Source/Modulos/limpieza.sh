3e3e3c5cd94b70325ee1b62fc0e4a2264d0e026e
rukshanchamindu <77233680+rukshanchamindu@users.noreply.github.com>
2021-02-12 15:55:58 +0530

    Delete VPSPACK-MOD_PWRMX directory

--- VPSPACK-MOD_PWRMX/VPSPack Mod v3.7.2 Source/Modulos/limpieza.sh
+++
@@ -1,8 +0,0 @@
-echo "Limpiando sistema y Reiniciando Servicios" 
-echo 3 > /proc/sys/vm/drop_caches 1> /dev/null 2> /dev/null 
-sysctl -w vm.drop_caches=3 1> /dev/null 2> /dev/null 
-swapoff -a && swapon -a 1> /dev/null 2> /dev/null 
-service ssh restart 1> /dev/null 2> /dev/null 
-service squid restart 1> /dev/null 2> /dev/null 
-service squid3 restart 1> /dev/null 2> /dev/null
-echo "Limpieza Finalizada"
