0e2fa358c20c99c3574d9cc775ec13e60e93808d
rukshanchamindu <77233680+rukshanchamindu@users.noreply.github.com>
2021-02-12 15:51:33 +0530

    Delete ADM-MANAGER-DANKELTHAHER directory

--- ADM-MANAGER-DANKELTHAHER/Dankelthaher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/ADM-MANAGER-DANKELTHAHER/instala.sh; chmod 777 instala.sh* && ./instala.sh*
-
